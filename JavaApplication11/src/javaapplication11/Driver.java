6bbd06ebe4ec4ac45abadcf0ab01378682d61fec
teddy_rocket <richardnguyen1234@users.noreply.github.com>
2020-07-05 19:24:53 -0400

    Update Driver.java

--- JavaApplication11/src/javaapplication11/Driver.java
+++ JavaApplication11/src/javaapplication11/Driver.java
@@ -1,36 +1,30 @@
+package javaapplication11;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication11;
 
 /**
  *
- * @author Le Cherng
+ * @author Le Cherng Lee, Mona Kia, Richard Nguyen
  */
 public class Driver {
 
-    /**
+    /** 
+     * main class
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*******************************************************************************************************************************************
-    	  * TODO : implement all the operations of main class   																					*
-    	  ******************************************************************************************************************************************/
-        
-        
+    	
     	Network objNetwork = new Network("network");            /* Activate the network */
-        synchronized(objNetwork){
-            objNetwork.start();
-            Server objServer = new Server();                        /* Start the server */ 
-            objServer.start();
-            Client objClient1 = new Client("sending");              /* Start the sending client */
-            objClient1.start();
-            Client objClient2 = new Client("receiving");            /* Start the receiving client */
-            objClient2.start();
-            objNetwork.notify();
-        }
+        objNetwork.start();
+        Server objServer = new Server();                        /* Start the server */ 
+        objServer.start();
+        Client objClient1 = new Client("sending");              /* Start the sending client */
+        objClient1.start();
+        Client objClient2 = new Client("receiving");            /* Start the receiving client */
+        objClient2.start();
     }
-    
 }
