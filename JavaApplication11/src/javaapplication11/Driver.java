d52e74c4f2637d41cb5922e9818128b759fe1465
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2020-07-05 01:12:42 -0400

    Slight update to client, driver and network class

--- JavaApplication11/src/javaapplication11/Driver.java
+++ JavaApplication11/src/javaapplication11/Driver.java
@@ -19,14 +19,18 @@ public class Driver {
     	  * TODO : implement all the operations of main class   																					*
     	  ******************************************************************************************************************************************/
         
+        
     	Network objNetwork = new Network("network");            /* Activate the network */
-        objNetwork.start();
-        Server objServer = new Server();                        /* Start the server */ 
-        objServer.start();
-        Client objClient1 = new Client("sending");              /* Start the sending client */
-        objClient1.start();
-        Client objClient2 = new Client("receiving");            /* Start the receiving client */
-        objClient2.start();
+        synchronized(objNetwork){
+            objNetwork.start();
+            Server objServer = new Server();                        /* Start the server */ 
+            objServer.start();
+            Client objClient1 = new Client("sending");              /* Start the sending client */
+            objClient1.start();
+            Client objClient2 = new Client("receiving");            /* Start the receiving client */
+            objClient2.start();
+            objNetwork.notify();
+        }
     }
     
 }
